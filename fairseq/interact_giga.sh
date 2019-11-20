a86c904d66cb1525374bf804ad334df2bb164bfb
Kazuki <marukaz.jh@gmail.com>
2019-11-20 15:21:44 +0900

    Update

--- fairseq/interact_giga.sh
+++ fairseq/interact_giga.sh
@@ -2,21 +2,21 @@
 ## current working directory
 #$ -cwd
 #$ -l h_rt=01:00:00
-#$ -N gen
+#$ -N interact_giga
 #$ -m abe
 #$ -M kopamaru@gmail.com
 
 ## Initialize module command (don't remove)
 . /etc/profile.d/modules.sh
 
+
 source ~/my_dir/venvs/fairseq/bin/activate
 
 beam=5; subset="test"; \
-model=$1; \
-data=$1; \
+DATA=$1; \
 # mkdir -p /gs/hs0/tga-nlp-titech/matsumaru/exp/giga/$data; \
-fairseq-generate /gs/hs0/tga-nlp-titech/matsumaru/data/giga/${data}_bin \
---path /gs/hs0/tga-nlp-titech/matsumaru/exp/giga/$model/checkpoint_best.pt \
---gen-subset $subset \
---batch-size 128 \
---beam ${beam} > /gs/hs0/tga-nlp-titech/matsumaru/exp/giga/$model/gen.txt
+fairseq-interactive ~/my_dir/data/giga/${DATA}_bin \
+--path ~/my_dir/exp/giga/$DATA/checkpoint_best.pt \
+--beam $beam \
+--remove-bpe " ##" --buffer-size 64 \
+--input ~/my_dir/data/giga/giga_1snt_small_test_wp/test.src > ~/my_dir/exp/giga/$DATA/gen_test10k.txt
